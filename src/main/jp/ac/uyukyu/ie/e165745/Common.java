4b2da19a85b0a467dea38944504bb76491de4a4f
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2017-02-01 15:44:03 +0900

    game making for report7

---
+++ src/main/jp/ac/uyukyu/ie/e165745/Common.java
@@ -0,0 +1,12 @@
+package main.jp.ac.uyukyu.ie.e165745;
+
+/**
+ * Created by e165745 on 2017/02/01.
+ */
+public interface Common {
+    // •ûŒü‚ð•\‚·’è”
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int UP = 2;
+    public static final int DOWN = 3;
+}
